bedff53aa08023a594ca0632c63a408a8342d7e4
yancey <wangyu.cn@gmail.com>
2024-11-28 10:46:53 -0500

    disable debug nand

--- comm.cpp
+++ comm.cpp
@@ -4,7 +4,7 @@ string inject_code;
 uint64_t tick=0;
 
 bool enable_dyn_debug=false;
-bool enable_debug_nand=true;
+bool enable_debug_nand=false;
 
 bool enable_debug_switch=false;
 bool enable_debug_pc=false;
