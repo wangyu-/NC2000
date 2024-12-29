30ef084f23ccb9956af49daf25009cdc6a43349d
yancey <yancey@whist.com>
2024-12-28 21:37:49 -0500

    clear and fix for nor

--- nor.cpp
+++ nor.cpp
@@ -19,7 +19,15 @@ static uint8_t& fp_type = nc1020_states.fp_type;
 static uint8_t nor_info_block[0x100]={
 0xdb,0xf0,0xd4,0xb6,0xbc,0xfb,'N','C','2','0','0','0',1,1,1,1,1,1,1,1
 };
-
+enum NOR_CMD{
+    NONE=0,
+    SW_ID=1,
+    BYTE_PROGRAM=2,
+    BLOCK_OR_MASS_ERASE=3,
+    INFO_BYTE_PROGRAM=4,
+    INFO_OR_BMASS_ERASE=5,
+    INFO_READ=6,
+};
 void LoadNor(){
 	uint8_t* temp_buff = (uint8_t*)malloc(NOR_SIZE);
 	FILE* file = fopen(nc1020_rom.norFlashPath.c_str(), "rb");
@@ -71,14 +79,14 @@ bool read_nor(uint16_t addr, uint8_t &value){
     // the "read any byte" on datasheet should mean "any byte in nor address"
     if (!in_nor_range(addr)) return false;
 
-    if(fp_type==6 && fp_step ==3){
+    if(fp_type==NOR_CMD::INFO_READ && fp_step ==3){
         printf("read fp_type=%d fp_step=%d addr=%04x\n",fp_type,fp_step,addr);
         value=nor_info_block[addr%0x100];
         return true;
     }
-    if (((fp_step == 4 && fp_type == 2) ||
-		(fp_step == 6 && fp_type == 3))) {
-		fp_step = 0;
+    if (((fp_type == NOR_CMD::BYTE_PROGRAM && fp_step == 4) ||
+		(fp_type == NOR_CMD::BLOCK_OR_MASS_ERASE && fp_step == 6))) {
+		//fp_step = 0; //this doesn't respect datasheet??
         value=0x88;
 		return true;
 	}
@@ -116,30 +124,34 @@ void write_nor0(uint16_t addr,uint8_t value){
     bool addr_is_0x5555 = (addr == 0x5555)||(addr == 0xd555);
 
     if (fp_step == 0) {
+        assert(fp_type==NOR_CMD::NONE);
         if (addr_is_0x5555 && value == 0xAA) {
             fp_step = 1;
+            return;
         }
-        return;
     }
     if (fp_step == 1) {
+        assert(fp_type==NOR_CMD::NONE);
         if (addr == 0xAAAA && value == 0x55) {
         	fp_step = 2;
             return;
         }
     } else if (fp_step == 2) {
+        assert(fp_type==NOR_CMD::NONE);
         if (addr_is_0x5555) {
+            fp_type=NOR_CMD::NONE;
         	switch (value) {
-        	case 0x90: fp_type = 1; break;
-        	case 0xA0: fp_type = 2; break;
-        	case 0x80: fp_type = 3; break;
-        	case 0xA8: fp_type = 4; break;
-        	case 0x88: fp_type = 5; break;
-        	case 0x78: fp_type = 6; break;
+        	case 0x90: fp_type = NOR_CMD::SW_ID; break;
+        	case 0xA0: fp_type = NOR_CMD::BYTE_PROGRAM ; break;
+        	case 0x80: fp_type = NOR_CMD::BLOCK_OR_MASS_ERASE; break;
+        	case 0xA8: fp_type = NOR_CMD::INFO_BYTE_PROGRAM; break;
+        	case 0x88: fp_type = NOR_CMD::INFO_OR_BMASS_ERASE; break;
+        	case 0x78: fp_type = NOR_CMD::INFO_READ; break;
             //default: printf("no new fp type\n");
         	}
             //printf("new fp type=%d\n",fp_type);
             if (fp_type) {
-                if (fp_type == 1) {
+                if (fp_type == NOR_CMD::SW_ID) {
 					assert(false);
                     //fp_bank_idx = bank_idx;
                     //fp_bak1 = bank[0x0000];
@@ -150,50 +162,54 @@ void write_nor0(uint16_t addr,uint8_t value){
             }
         }
     } else if (fp_step == 3) {
-        if (fp_type == 1) {
+        if (fp_type == NOR_CMD::SW_ID) {
+            assert(false);
+            /*
             if (value == 0xF0) {
                 //bank[0x0000] = fp_bak1;
                 //bank[0x0001] = fp_bak2;
                 fp_step = 0;
                 fp_type = 0;
                 return;
-            }
-        } else if (fp_type == 2) {
+            }*/
+        } else if (fp_type == NOR_CMD::BYTE_PROGRAM) {
             memmap[addr>>13][addr&0x1fff] &= value;
             fp_step = 4;
             return;
-        } else if (fp_type == 4) {
+        } else if (fp_type == NOR_CMD::INFO_BYTE_PROGRAM) {
             nor_info_block[addr & 0xFF] &= value;
             fp_step = 4;
             return;
-        } else if (fp_type == 3 || fp_type == 5) {
+        } else if (fp_type == NOR_CMD::BLOCK_OR_MASS_ERASE || fp_type == NOR_CMD::INFO_OR_BMASS_ERASE) {
             if (addr_is_0x5555 && value == 0xAA) {
                 fp_step = 4;
                 return;
             }
         }
     } else if (fp_step == 4) {
-        if (fp_type == 3 || fp_type == 5) {
+        if (fp_type == NOR_CMD::BLOCK_OR_MASS_ERASE || fp_type == NOR_CMD::INFO_OR_BMASS_ERASE) {
             if (addr == 0xAAAA && value == 0x55) {
                 fp_step = 5;
                 return;
             }
         }
     } else if (fp_step == 5) {
-		
+		assert(fp_type== NOR_CMD::BLOCK_OR_MASS_ERASE||fp_type== NOR_CMD::INFO_OR_BMASS_ERASE);
         if (addr_is_0x5555 && value == 0x10) {
+            //if write to 0x5555 then it's MASS_ERASE or BMASS_ERASE
         	for (uint32_t i=0; i<num_nor_pages; i++) {
 				printf("wanna erase all\n");
                 memset(nor_banks[i], 0xFF, 0x8000);
             }
             if (fp_type == 5) {
-                printf("wanna erase infoblock size 256 A\n");
+                //if it's BMASS erase info block in addition erase info block
+                printf("wanna erase infoblock size 256\n");
                 memset(nor_info_block, 0xFF, 0x100);
             }
             fp_step = 6;
             return;
         }
-        if (fp_type == 3) {
+        else if (fp_type == BLOCK_OR_MASS_ERASE) {
             if (value == 0x30) {
                 if(nc2000mode||nc1020mode){
                     printf("wanna erase size 2048, addr= %04x\n",addr);
@@ -206,9 +222,9 @@ void write_nor0(uint16_t addr,uint8_t value){
                 fp_step = 6;
                 return;
             }
-        } else if (fp_type == 5) {
-            if (value == 0x48) {
-                printf("wanna erase size 256 B\n");
+        } else if (fp_type == INFO_OR_BMASS_ERASE) {
+            if (value == 0x30) {
+                printf("wanna erase infoblock size 256 B\n");
                 memset(nor_info_block, 0xFF, 0x100);
                 fp_step = 6;
                 return;
